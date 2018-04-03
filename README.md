46f56a661c935117bc7056a5ee7e6cd83a5c9aba
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2018-04-03 18:14:22 +0900

    migrate to git

---
+++ README.md
@@ -0,0 +1,80 @@
+﻿# gulp-colorfulkits
+Make gulp log colorful
+```
+WARNING: this module require many unwanted modules.
+it's grab-bag of modules.
+so, not published yet. currently private.
+```
+
+## Usage
+```sh
+npm i git+https://github.com/mochiya98/gulp-colorfulkits#master
+```
+```javascript
+const {
+  colorful,
+  gulpColorfulEslint,
+  gulpWatchColorful,
+  watchColorful,
+} = require("gulp-colorfulkits");
+```
+
+## colorful(options)
+```javascript
+gulp.src('./src/*.js')
+  .pipe(colorful())//Output Colorful DestLog
+  .pipe(gulp.dest('./dest/'));
+```
+#### [object]options = {color, cwd, indent}
+- [string]color
+  - [ansi-colors](https://github.com/doowb/ansi-colors) text-color enum
+  - default:`cyan`
+- [string]cwd
+  - [vinyl](https://github.com/gulpjs/vinyl) cwd
+  - default:`.`
+- [integer]indent
+  - indent-level
+  - default:`1`
+
+## colorful.preset(options);
+```javascript
+const customColorful = colorful.preset({color:"green"});
+gulp.src('./src/**/*.js')
+  .pipe(customColorful())
+  .pipe(gulp.dest('./dest/'));
+```
+#### [object]options
+same
+
+## gulpColorfulEslint()
+```javascript
+gulp.src('./src/**/*.js')
+  .pipe(gulpColorfulEslint())
+  //like .pipe(eslint()).pipe(eslint.result())
+```
+
+## gulpWatchColorful(glob, callback, options)
+```javascript
+gulpWatchColorful("./src/**/*.js",function(){
+  //callback
+});
+//like gulp.watch(glob,callback)
+```
+- [string]glob
+  - [glob]([https://github.com/isaacs/node-glob)
+- [function]callback
+  - called when file changed.
+- [object]options
+  - [boolean]ignoreDirectoryEvents
+    - default `true`
+
+
+## watchColorful(glob)
+```javascript
+watchColorful("./src/**/*.js")
+  .pipe(gulp.dest("./dest/"));
+//like require("gulp-watch")(glob)
+```
+- [string]glob
+  - [glob]([https://github.com/isaacs/node-glob)
+
---
+++ index.js
@@ -0,0 +1,107 @@
+const fs = require("fs");
+const path = require("path");
+const {Transform} = require("stream");
+
+const lazypipe = require("lazypipe");
+
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+
+const eslint = require("gulp-eslint");
+const watch = require("gulp-watch");
+const log = require("fancy-log");
+const colors = require("ansi-colors");
+
+const colorful = function(opts){
+	opts.color = opts.color || "cyan";
+	opts.cwd = opts.cwd || ".";
+	opts.indent = opts.indent === void 0 ? 1 : opts.indent;
+	let indent = "  ".repeat(opts.indent);
+	//let log_header = indent+"-> \x1b[1m\x1b["+colors[opts.color]+"m";
+	let transform = function(file, encoding, callback){
+		if(file.isNull())return callback(null, file);
+		let fp = opts.cwd
+			? path.resolve(
+				opts.cwd,
+				path.relative(file.base, file.path)
+			)
+			: file.path;
+		log(`${indent} -> ${colors.bold(colors[opts.color](fp))}`);
+		//console.log(log_header+fp+"\x1b[0m");
+		this.push(file);
+		return callback();
+	};
+	return new Transform({
+		objectMode: true,
+		transform,
+	});
+};
+colorful.preset = function(opts){
+	return function(){
+		return colorful(opts);
+	};
+};
+
+const gulpColorfulEslint = lazypipe()
+	.pipe(eslint)
+	.pipe(
+		eslint.result,
+		res => {
+			if(res.messages.length === 0)return;
+			log(
+				colors.bold(colors.bgBlue(
+					`ESLint: ${res.filePath}`
+				))
+			);
+			for(let msg of res.messages){
+				log(
+					colors.bold(colors[[null, "yellow", "red"][msg.severity]](
+						`  ${msg.line}:${msg.column} ${msg.message} (${msg.ruleId})`
+					))
+				);
+			}
+		}
+	);
+
+const gulpWatchColorful = function(globPath, func, opts){
+	if(!opts) opts = {};
+	if(!opts.ignoreDirectoryEvents) opts.ignoreDirectoryEvents = true;
+	return gulp.watch(globPath, function(...args){
+		if(opts.ignoreDirectoryEvents && fs.statSync(args[0].path).isDirectory())return;
+		log(colors.bold({
+			"added"  : colors.magenta("add  "),
+			"changed": colors.yellow("change"),
+			"deleted": colors.red("delete"),
+			"renamed": colors.red("rename"),
+		}[args[0].type])
+			+ ": "
+			+ colors.bold(colors.green(args[0].path)));
+		func(...args);
+	});
+};
+
+const watchColorful = function(...args){
+	return watch(...args)
+		.pipe(new Transform({
+			objectMode: true,
+			transform : function(evt, encoding, callback){
+				if(evt.isNull())return callback(null, evt);
+				log(colors.bold({
+					"add"   : colors.magenta("add  "),
+					"change": colors.yellow("change"),
+					"unlink": colors.red("delete"),
+				}[evt.event])
+					+ ": "
+					+ colors.bold(colors.green(evt.path)));
+				this.push(evt);
+				return callback();
+			},
+		}));
+};
+
+module.exports = {
+	colorful, //.pipe(colorful());
+	gulpColorfulEslint, //like .pipe(eslint).pipe(eslint.result)
+	gulpWatchColorful, //like gulp.watch("glob",()=>{});
+	watchColorful, //like require("gulp-watch")(...args);
+};
